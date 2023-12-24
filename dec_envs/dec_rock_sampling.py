c3f384a929e7ce057b360f2186e7181124a64dc2
Yoav Elem <132524745+elemyoav@users.noreply.github.com>
2023-12-24 19:58:04 +0200

    Update dec_rock_sampling.py

--- dec_envs/dec_rock_sampling.py
+++ dec_envs/dec_rock_sampling.py
@@ -3,7 +3,7 @@ import math
 import random
 
 class DecRockSampling(MultiAgentEnv):
-    def __init__(self, width=2, height=2, bad_rocks=1, good_rocks=1, horizon = 20):
+    def __init__(self, width=2, height=2, bad_rocks=1, good_rocks=1, horizon=20):
         self.width = width
         self.height = height
         self.good_rocks = good_rocks
@@ -17,11 +17,11 @@ class DecRockSampling(MultiAgentEnv):
         self.good_rocks_positions = [self.init_random_location() for _ in range(good_rocks)]
         self.bad_rocks_positions = [self.init_random_location() for _ in range(bad_rocks)]
 
-        self.observations = None
+        self.observations = []
 
     def init_random_location(self):
-        x = random.randint(0, self.width)
-        y = random.randint(0, self.height)
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height - 1)
         return (x, y)
 
     def move_rover1(self, direction):
@@ -57,18 +57,18 @@ class DecRockSampling(MultiAgentEnv):
         else:
             return 'none'
 
-
     def sample(self, rover_position):
         x, y = rover_position
         rock_quality = self.sense(rover_position)
         if rock_quality == 'good':
             self.good_rocks_positions.remove((x, y))
-            self.bad_rocks_positions.add((x, y))
+            self.bad_rocks_positions.append((x, y))
             self.sampled_rocks += 1
+            if self.sampled_rocks == self.good_rocks:
+                return 750
+            return 0
         elif rock_quality == 'bad':
             return -500
-        if sampled_rocks == self.good_rocks:
-                return 750
         else:
             return 0
 
@@ -77,99 +77,19 @@ class DecRockSampling(MultiAgentEnv):
         for action in actions:
             if action[0] == "no-op":
                 continue
-
-            elif action[0] == "move":  # action[1] = rover_id, action[2] = direction
+            elif action[0] == "move":
                 total_reward -= 1
                 if action[1] == 0:
                     self.move_rover1(action[2])
                 elif action[1] == 1:
                     self.move_rover2(action[2])
-
-            elif action[0] == "sense":  # action[1] = rover_id
+            elif action[0] == "sense":
                 total_reward -= 5
                 if action[1] == 0:
-                    self.observations.add([self.sense(self.rover1_position), rover1_position])
+                    self.observations.append([self.sense(self.rover1_position), self.rover1_position])
                 elif action[1] == 1:
-                    self.observations.add([self.sense(self.rover2_position), rover2_position])
-
-            elif action[0] == "sample":  # action[1] = rover_id
+                    self.observations.append([self.sense(self.rover2_position), self.rover2_position])
+            elif action[0] == "sample":
                 if action[1] == 0:
                     total_reward += self.sample(self.rover1_position)
-                elif action[1] == 1:
-                    total_reward += self.sample(self.rover2_position)
-
-        return total_reward
-
-
-    def step(self, actions):     
-        self.step_count += 1
-        if self.step_count >= self.horizon:
-            reward = 0
-            self.step_count = 0
-            return reward, done
-        
-        actions = self.translate_actions(actions)
-        reward = self.execute_actions(actions)
-        return reward, done
-
-    def translate_actions(self, actions):
-        translated_actions = []
-        noop = [0]
-        move1 = [1, 2, 3, 4]
-        move2 = [5, 6, 7, 8]
-        sense = [9, 10]
-        sample = [11, 12]
-
-        for action in actions:
-            if action in noop:
-                translated_actions.append(("no-op",))
-            elif action in move1:
-                if action == 1:
-                    translated_actions.append(("move", 0, "left"))
-                elif action == 2:
-                    translated_actions.append(("move", 0, "right"))
-                elif action == 3:
-                    translated_actions.append(("move", 0, "up"))
-                elif action == 4:
-                    translated_actions.append(("move", 0, "down"))
-            elif action in move2:
-                if action == 5:
-                    translated_actions.append(("move", 1, "left"))
-                elif action == 6:
-                    translated_actions.append(("move", 1, "right"))
-                elif action == 7:
-                    translated_actions.append(("move", 1, "up"))
-                elif action == 8:
-                    translated_actions.append(("move", 1, "down"))
-            
-            elif action in sense:
-                if action == 9:
-                    translated_actions.append(("sense", 0))
-                elif action == 10:
-                    translated_actions.append(("sense", 1))
-            
-            elif action in sample:  
-                if action == 11:
-                    translated_actions.append(("sample", 0))
-                elif action == 12:
-                    translated_actions.append(("sample", 1))
-
-            else:
-                raise IndexError
-
-        return translated_actions
-
-    def get_obs(self):
-        old_observations =  self.observations.copy()
-        self.observations = []
-        return [old_observations]
-
-    def reset(self):
-        self.rover1_position = (0, 0)
-        self.rover2_position = (0, self.width - 1)
-        self.sampled_rocks = 0
-        self.step_count = 0
-        self.good_rocks_positions = [self.init_random_location() for _ in range(self.good_rocks)]
-        self.bad_rocks_positions = [self.init_random_location() for _ in range(self.bad_rocks)]
-        self.observations = []
-        return self.get_obs()
+               
